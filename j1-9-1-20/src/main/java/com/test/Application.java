1ddc960cb44ff8ad569ac422f6adff1cd65d9f19
laileou <2132392010@qq.com>
2024-04-06 08:52:05 +0800

    hot-fix commit1

--- j1-9-1-20/src/main/java/com/test/Application.java
+++ j1-9-1-20/src/main/java/com/test/Application.java
@@ -15,7 +15,7 @@ public class Application {
         String reply = messageService.send("今天天气怎么样");
         System.out.println(reply);
 
-        if (reply.indexOf("查询哪个城市") != -1) {
+        if (reply.indexOf("查询哪个城市") != -1) {//indexof查找字符串所在位置，找不到返回-1
             reply = messageService.send("杭州");
             System.out.println(reply);
         }
--- j1-9-1-20/src/main/java/com/test/model/RobotMessageRequest.java
+++ j1-9-1-20/src/main/java/com/test/model/RobotMessageRequest.java
@@ -10,16 +10,25 @@ public class RobotMessageRequest {
     private UserInfo userInfo = new UserInfo();
 
     //补充类成员变量的Getter and Setter方法
-    public  String Getter(){
-        return perception.getInputText().text;
-    }
 
-    public static void Setter(){
-
-    }
     public void setPerception(Perception per){
         this.perception=per;
     }
+    public void setUserInfo(UserInfo user){
+        this.userInfo=user;}
+    public void setReqType(int req){
+        this.reqType=req;
+    }
+    public Perception getPerception(){
+        return perception;
+    }
+    public UserInfo getUserInfo(){
+        return userInfo;
+    }
+    public int getReqType() {
+        return reqType;
+    }
+
     public  class Perception{
 
         private InputText inputText;
@@ -50,6 +59,12 @@ public class RobotMessageRequest {
         private String userId = "youkeda001";
 
         //完善类的剩余结构   提示：仔细思考一下这个类需要完成的额一些业务逻辑
+        public UserInfo(String Key,String Id)
+        {
+            this.apiKey=Key;
+            this.userId=Id;
+        }
+
     }
 
 }
